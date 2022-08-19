33991814810f5e4a1949f8af007b33ffbaa9b1b0
Regina Azzi <reginaazzi@hotmail.com>
2022-08-19 10:55:58 -0300

    alteracao das pastas

---
+++ Exercicio Javascript/PareImpar.js
@@ -0,0 +1,15 @@
+// Recebe uma quantidade de valores para avaliar
+// funçao exibe se cada valor é par ou impar
+
+exibirTipo(5);
+
+function exibirTipo(limite){
+    for (let i = 0; i <= limite; i++){
+        if (i % 2 === 0)
+        console.log (i,'Par');
+        else
+        console.log (i,'Impar');
+
+    }
+}
+
---
+++ Exercicio Javascript/Readme.md
@@ -0,0 +1,5 @@
+Projeto: Logical_Forest Sprint 4
+
+Apresentação
+Na branch main é a branch principal, no final da sprint todo o projeto sera comitado para branch main
+Na branch develop será feito os commit dos exercicios diarios.
