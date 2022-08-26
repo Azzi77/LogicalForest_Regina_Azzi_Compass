89e751b4aaa1b764befc7de36e56b42ca30a7be3
Regina Azzi <reginaazzi@hotmail.com>
2022-08-26 17:14:03 -0300

    ultimas alterações

--- MochaChai/Test/sampleChai.spec.js
+++ MochaChai/Test/sampleChai.spec.js
@@ -3,17 +3,15 @@ import chai from 'chai'
 
 const expect = chai.expect
 
-describe (' Meu primeiro teste ', () => {
-
-    it.only('Verificar a variavel "aux"', () => {
+describe('Meu primeiro teste ', () => {
+    it('Verificar a variavel "aux"', () => {
         let aux = 2
         expect(aux).to.be.equals(2).and.to.be.a('number')
-    
-})       
-    it.only ('Verificar outra variavel ', () => {
-    let aux = 'carro'
-    expect(aux).to.be.equals('carro').and.not.equals('charrete')
-       
 
-})
+    })
+    it('Verificar outra variavel ', () => {
+        let aux = 'carro'
+        expect(aux).to.be.equals('carro').and.not.equals('charrete')
+
+    })
 })
--- MochaChai/Test/sampleMocha.spec.js
+++ MochaChai/Test/sampleMocha.spec.js
@@ -1,38 +1,35 @@
 import assert from 'assert'
 
-
-describe (' Meu primeiro teste', () =>{
-
-    it ('Verificar uma igualdade', ()=>{
+describe(' Meu primeiro teste', () => {
+    it('Verificar uma igualdade', () => {
         let aux = 2
         assert.strictEqual(aux, 2)
     })
-
-    it ('Verificar uma desigualdade', ()=> {
-        let aux =2
-        assert.strictEqual(aux,3)
-    })
-    it ('Verificar uma desigualdade', ()=> {
-        let aux =2
-        assert.notStrictEqual(aux,3) 
-    })
-    it ('Verificar outra igualdade', ()=> {
-        let aux = 'carro'
-        assert.strictEqual(aux,'carro')
-})
-})
-
-describe ('Meu segundo teste' , ()=>{
-    
-    it ('Verificar  desigualdade', () =>{
+    it('Verificar uma desigualdade', () => {
         let aux = 2
-        assert.notStrictEqual(aux,'2')
+        assert.strictEqual(aux, 3)
+    })
+    it('Verificar uma desigualdade', () => {
+        let aux = 2
+        assert.notStrictEqual(aux, 3)
+    })
+    it('Verificar outra igualdade', () => {
+        let aux = 'carro'
+        assert.strictEqual(aux, 'carro')
+    })
 
 })
-it ('Verificar outra desigualdade', () =>{
-    let aux = 'carro'
-    assert.notStrictEqual(aux,'charrete')
+
+describe('Meu segundo teste', () => {
+    it('Verificar  desigualdade', () => {
+        let aux = 2
+        assert.notStrictEqual(aux, '2')
+
+    })
+    it('Verificar outra desigualdade', () => {
+        let aux = 'carro'
+        assert.notStrictEqual(aux, 'charrete')
 
 
-})
+    })
 })
